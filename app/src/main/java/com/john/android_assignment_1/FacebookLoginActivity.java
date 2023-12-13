b31ce9c0e5d87da47bb6278df67f5dd84436c1c3
John Safwat <john362sss@gmail.com>
2023-12-13 17:55:04 +0200

    Create Android Project

---
+++ app/src/main/java/com/john/android_assignment_1/FacebookLoginActivity.java
@@ -0,0 +1,14 @@
+package com.john.android_assignment_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FacebookLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_login);
+    }
+}
