73b44059fd079e1546e2ab897966345364ca02ab
John Safwat <john362sss@gmail.com>
2024-01-19 21:26:50 +0200

    update the facebook time line using recyclerview
    + Modify activity_facebook_post.xml
    + Add activity_facebook_recyclerview.xml
    + Modify AndroidManifest.xml
    + Add appInsightsSettings.xml
    + Modify build.gradle.kts
    + Modify FacebookLoginActivity.java
    + Delete FacebookPostActivity.java
    + Add FaceBookPostDTO.kt
    + Add FacebookPostsTimeLineAdapter.kt
    + Add FacebookTimeLine.kt

--- app/src/main/java/com/john/android_assignment_1/FacebookLoginActivity.java
+++ app/src/main/java/com/john/android_assignment_1/FacebookLoginActivity.java
@@ -2,13 +2,24 @@ package com.john.android_assignment_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.john.android_assignment_1.databinding.ActivityFacebookLoginBinding;
+import com.john.android_assignment_1.databinding.ActivityFacebookPostBinding;
 
 public class FacebookLoginActivity extends AppCompatActivity {
 
+    ActivityFacebookLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_facebook_login);
+        binding = ActivityFacebookLoginBinding.inflate(getLayoutInflater());
+        binding.loginButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this , FacebookTimeLine.class);
+            startActivity(intent);
+            finish();
+        });
+        setContentView(binding.getRoot());
     }
 }
